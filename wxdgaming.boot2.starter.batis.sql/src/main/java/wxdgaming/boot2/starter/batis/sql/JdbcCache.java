065f411029fc2e86aa517fce2dbdf555c199d270
wxdgaming <492794628@qq.com>
2025-02-23 18:38:25 +0800

    【优化】命名结构

--- wxdgaming.boot2.starter.batis.sql/src/main/java/wxdgaming/boot2/starter/batis/sql/JdbcCache.java
+++ wxdgaming.boot2.starter.batis.sql/src/main/java/wxdgaming/boot2/starter/batis/sql/JdbcCache.java
@@ -15,12 +15,12 @@ import java.util.concurrent.TimeUnit;
  * @version: 2025-02-17 11:28
  **/
 @Getter
-public class JdbcCache<E extends Entity, UID> {
+public class JdbcCache<E extends Entity, Key> {
 
     protected final Class<E> cls;
     protected final TableMapping tableMapping;
     protected final SqlDataHelper<?> sqlDataHelper;
-    protected final Cache<UID, E> cache;
+    protected final Cache<Key, E> cache;
 
     /**
      * 构建
@@ -32,7 +32,7 @@ public class JdbcCache<E extends Entity, UID> {
         this.cls = ReflectContext.getTClass(this.getClass());
         this.sqlDataHelper = sqlDataHelper;
         this.tableMapping = this.sqlDataHelper.tableMapping(cls);
-        cache = Cache.<UID, E>builder()
+        cache = Cache.<Key, E>builder()
                 .cacheName("cache-" + tableMapping.getTableName())
                 .expireAfterAccess(expireAfterAccessM, TimeUnit.MINUTES)
                 .heartTime(1, TimeUnit.MINUTES)
@@ -42,8 +42,8 @@ public class JdbcCache<E extends Entity, UID> {
                 .build();
     }
 
-    protected E loader(UID uid) {
-        E byId = (E) sqlDataHelper.findByKey(cls, uid);
+    protected E loader(Key key) {
+        E byId = (E) sqlDataHelper.findByKey(cls, key);
         if (byId != null) {
             byId.setNewEntity(false);
             byId.checkHashCode();
@@ -51,37 +51,46 @@ public class JdbcCache<E extends Entity, UID> {
         return byId;
     }
 
-    protected void heart(UID uid, E e) {
+    protected void heart(Key key, E e) {
         boolean checkHashCode = e.checkHashCode();
         if (checkHashCode) {
             sqlDataHelper.getDataBatch().save(e);
         }
     }
 
-    protected boolean removed(UID uid, E e) {
+    protected boolean removed(Key key, E e) {
         sqlDataHelper.update(e);
         return true;
     }
 
+    /** 是否包含kay */
+    public boolean containsKey(Key k) {
+        return cache.containsKey(k);
+    }
 
     /** 如果获取数据null 抛出异常 */
-    public E get(UID key) {
+    public E get(Key key) {
         return cache.get(key);
     }
 
     /** 获取数据，如果没有数据返回null */
-    public E getIfPresent(UID ID) {
+    public E getIfPresent(Key ID) {
         return cache.getIfPresent(ID);
     }
 
-    public void put(UID key, E value) {
+    /** 如果数据不存在，不会加载数据库，返回null */
+    public E find(Key ID) {
+        return cache.getIfPresent(ID, null);
+    }
+
+    public void put(Key key, E value) {
         sqlDataHelper.save(value);
         value.setNewEntity(false);
         cache.put(key, value);
     }
 
     /** 强制缓存过期 */
-    public void invalidate(UID key) {
+    public void invalidate(Key key) {
         cache.invalidate(key);
     }
 
