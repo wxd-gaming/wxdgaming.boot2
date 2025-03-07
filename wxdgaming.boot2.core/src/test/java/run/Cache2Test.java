1c7fe3a081c2891b51f93338677459ac6df740d2
wxd-gaming <492794628@qq.com>
2025-03-07 14:47:30 +0800

    【优化】cache增量问题，其性能并不高

--- wxdgaming.boot2.core/src/test/java/run/Cache2Test.java
+++ wxdgaming.boot2.core/src/test/java/run/Cache2Test.java
@@ -2,7 +2,7 @@ package run;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.core.LogbackUtil;
-import wxdgaming.boot2.core.cache.Cache;
+import wxdgaming.boot2.core.cache.Cache2;
 import wxdgaming.boot2.core.lang.DiffTime;
 import wxdgaming.boot2.core.threading.ExecutorConfig;
 import wxdgaming.boot2.core.threading.ExecutorUtil;
@@ -16,7 +16,7 @@ import java.util.concurrent.CountDownLatch;
  * @author: wxd-gaming(無心道, 15388152619)
  * @version: 2025-03-07 10:47
  **/
-public class CacheTest {
+public class Cache2Test {
 
     public static void main(String[] args) throws InterruptedException {
 
@@ -31,7 +31,7 @@ public class CacheTest {
     }
 
     public static void t0() throws InterruptedException {
-        Cache<Long, Long> cache = Cache.<Long, Long>builder()
+        Cache2<Long, Long> cache = Cache2.<Long, Long>builder()
                 .cacheName("test")
                 .expireAfterWrite(1000)
                 .loader((k) -> k)
@@ -49,12 +49,12 @@ public class CacheTest {
             Long ifPresent = cache.getIfPresent(RandomUtils.random(max));
         }
         float diff = diffTime.diff();
-        System.out.println("Cache 单线程 随机访问：" + cache.cacheSize() + ", " + c1 + " 次 耗时:" + diff + " ms");
+        System.out.println("Cache2 单线程 随机访问：" + cache.cacheSize() + ", " + c1 + " 次 耗时:" + diff + " ms");
         cache.shutdown();
     }
 
     public static void t1() throws InterruptedException {
-        Cache<Long, Long> cache = Cache.<Long, Long>builder()
+        Cache2<Long, Long> cache = Cache2.<Long, Long>builder()
                 .cacheName("test")
                 .expireAfterWrite(1000)
                 .loader((k) -> k)
@@ -77,7 +77,7 @@ public class CacheTest {
         }
         latch.await();
         float diff = diffTime.diff();
-        System.out.println("Cache 多线程 随机访问：" + cache.cacheSize() + ", " + c1 + " 次 耗时:" + diff + " ms");
+        System.out.println("Cache2 多线程 随机访问：" + cache.cacheSize() + ", " + c1 + " 次 耗时:" + diff + " ms");
         cache.shutdown();
     }
 
