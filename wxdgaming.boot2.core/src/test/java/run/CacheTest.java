286df012fbb02b28e1f47555d77f4fe23d25fef9
wxd-gaming <492794628@qq.com>
2025-03-07 14:17:02 +0800

    【新增】cache测试

--- wxdgaming.boot2.core/src/test/java/run/CacheTest.java
+++ wxdgaming.boot2.core/src/test/java/run/CacheTest.java
@@ -25,6 +25,9 @@ public class CacheTest {
         t0();
         t0();
         t0();
+        t1();
+        t1();
+        t1();
     }
 
     public static void t0() throws InterruptedException {
@@ -32,24 +35,49 @@ public class CacheTest {
                 .cacheName("test")
                 .expireAfterWrite(1000)
                 .loader((k) -> k)
+                .removalListener((k, v) -> true)
                 .heartTime(10)
                 .hashArea(1000)
                 .build();
 
         DiffTime diffTime = new DiffTime();
 
-        int c1 = 1000_0000;
+        int c1 = 100_0000;
+        long max = 65535;
+
+        for (int i = 0; i < c1; i++) {
+            Long ifPresent = cache.getIfPresent(RandomUtils.random(max));
+        }
+        float diff = diffTime.diff();
+        System.out.println("单线程 随机访问：" + cache.cacheSize() + ", " + c1 + " 次 耗时:" + diff + " ms");
+        cache.shutdown();
+    }
+
+    public static void t1() throws InterruptedException {
+        Cache<Long, Long> cache = Cache.<Long, Long>builder()
+                .cacheName("test")
+                .expireAfterWrite(1000)
+                .loader((k) -> k)
+                .removalListener((k, v) -> true)
+                .heartTime(10)
+                .hashArea(1000)
+                .build();
+
+        DiffTime diffTime = new DiffTime();
+
+        int c1 = 100_0000;
+        long max = 65535;
         CountDownLatch latch = new CountDownLatch(c1);
 
         for (int i = 0; i < c1; i++) {
             ExecutorUtil.getInstance().getLogicExecutor().execute(() -> {
-                Long ifPresent = cache.getIfPresent((long) RandomUtils.random(65535));
+                Long ifPresent = cache.getIfPresent(RandomUtils.random(max));
                 latch.countDown();
             });
         }
         latch.await();
         float diff = diffTime.diff();
-        System.out.println("diff:" + diff + " - " + cache.cacheSize());
+        System.out.println("多线程 随机访问：" + cache.cacheSize() + ", " + c1 + " 次 耗时:" + diff + " ms");
         cache.shutdown();
     }
 
