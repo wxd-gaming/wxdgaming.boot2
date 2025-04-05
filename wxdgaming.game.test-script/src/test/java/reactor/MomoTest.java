89fd9dad269f859e59d8f3dbeb9973a1e220cf44
wxdgaming <492794628@qq.com>
2025-04-05 13:34:50 +0800

    【新增】reactor测试

---
+++ wxdgaming.game.test-script/src/test/java/reactor/MomoTest.java
@@ -0,0 +1,56 @@
+package reactor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import wxdgaming.boot2.core.threading.ExecutorUtil;
+
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * 数据流
+ *
+ * @author: wxd-gaming(無心道, 15388152619)
+ * @version: 2025-03-29 22:10
+ **/
+@Slf4j
+public class MomoTest {
+
+    static {
+        ExecutorUtil.getInstance().init();
+    }
+
+    public Mono<String> getUserInfo(String userId) {
+        return Mono.just("userInfo");
+    }
+
+    public Mono<String> getUserInfo1(String userId) {
+        CompletableFuture<String> stringCompletableFuture = ExecutorUtil.getInstance().getLogicExecutor().completableFuture(() -> {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return "userInfo";
+        });
+        return Mono.fromFuture(stringCompletableFuture);
+    }
+
+
+    @Test
+    public void t0() throws Exception {
+        Mono.delay(Duration.ofSeconds(1)).map(v -> "ss").subscribe(log::info);
+
+        // 异步延迟示例
+        Flux.interval(Duration.ofSeconds(1))
+                .take(5)
+                .subscribe(v -> log.info("{}", v));
+
+        getUserInfo("userId").subscribe(log::info);
+        getUserInfo1("userId").subscribe(log::info);
+        Thread.sleep(10000);
+    }
+
+}
