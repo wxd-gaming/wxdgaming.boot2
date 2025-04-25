aaec8d483b3845fca89437bccba5a268a136ea03
wxd-gaming <492794628@qq.com>
2025-04-25 15:45:01 +0800

    【新增】字符串切割测试

--- wxdgaming.boot2.core/src/test/java/code/string/StringSplitTest.java
+++ wxdgaming.boot2.core/src/test/java/code/string/StringSplitTest.java
@@ -5,20 +5,22 @@ import org.junit.jupiter.api.RepeatedTest;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.StringJoiner;
 import java.util.StringTokenizer;
 
 public class StringSplitTest {
 
-    static String delim = ":";
+    static final String delim = "ac";
+    static final String source = new StringJoiner(delim).add("mongo").add("key").add("2").add("test").toString();
 
     @Test
     @RepeatedTest(10)
     public void stringSplit() {
         long start = System.nanoTime();
         for (int i = 0; i < 1000000; i++) {
-            String[] s = "mongo:key:2:test".split(delim);
+            String[] s = source.split(delim);
         }
-        System.out.printf("%20s %7.2f ms%n", "String.Split", (System.nanoTime() - start) / 10000 / 100f);
+        System.out.printf("%24s %7.2f ms%n", "String.Split", (System.nanoTime() - start) / 10000 / 100f);
     }
 
     /** 按照单字符切割最快性能最好 */
@@ -27,12 +29,12 @@ public class StringSplitTest {
     public void stringTokenizer() {
         long start = System.nanoTime();
         for (int i = 0; i < 1000000; i++) {
-            StringTokenizer stringTokenizer = new StringTokenizer("mongo:key:2:test", delim);
+            StringTokenizer stringTokenizer = new StringTokenizer(source, delim);
             while (stringTokenizer.hasMoreTokens()) {
                 String string = stringTokenizer.nextToken();
             }
         }
-        System.out.printf("%20s %7.2f ms%n", "stringTokenizer", (System.nanoTime() - start) / 10000 / 100f);
+        System.out.printf("%24s %7.2f ms%n", "stringTokenizer", (System.nanoTime() - start) / 10000 / 100f);
     }
 
     /** 综合性能最好，最灵活 */
@@ -41,19 +43,48 @@ public class StringSplitTest {
     public void diySplit() {
         long start = System.nanoTime();
         for (int i = 0; i < 1000000; i++) {
-            List<String> s = new StringSplit("mongo:key:2:test", delim).getList();
+            List<String> s = new StringSplit(source, delim).getList();
         }
-        System.out.printf("%20s %7.2f ms%n", "new StringSplit", (System.nanoTime() - start) / 10000 / 100f);
+        System.out.printf("%24s %7.2f ms%n", "new StringSplit", (System.nanoTime() - start) / 10000 / 100f);
+    }
+
+    public void staticStringSplit() {
+        long start = System.nanoTime();
+        for (int i = 0; i < 1000000; i++) {
+            List<String> s = StringSplit.split(source, delim);
+        }
+        System.out.printf("%24s %7.2f ms%n", "static StringSplit", (System.nanoTime() - start) / 10000 / 100f);
+    }
+
+    public void staticStringSplit4Char() {
+        long start = System.nanoTime();
+        for (int i = 0; i < 1000000; i++) {
+            List<String> s = StringSplit4Char.manualSplit(source, delim.charAt(0));
+        }
+        System.out.printf("%24s %7.2f ms%n", "static StringSplit4Char", (System.nanoTime() - start) / 10000 / 100f);
     }
 
     @Test
     @RepeatedTest(10)
-    public void diySplit2() {
+    public void stringSplit4Char() {
         long start = System.nanoTime();
         for (int i = 0; i < 1000000; i++) {
-            List<String> s = StringSplit.split("mongo:key:2:test", delim);
+            List<String> s = new StringSplit4Char(source, delim).getList();
         }
-        System.out.printf("%20s %7.2f ms%n", "static StringSplit", (System.nanoTime() - start) / 10000 / 100f);
+        System.out.printf("%24s %7.2f ms%n", "new StringSplit4Char", (System.nanoTime() - start) / 10000 / 100f);
+    }
+
+    @Test
+    @RepeatedTest(10)
+    public void stringCharIterable() {
+        long start = System.nanoTime();
+        for (int i = 0; i < 1000000; i++) {
+            StringSplit4CharIterable stringSplit4CharIterable = new StringSplit4CharIterable(source, delim);
+            while (stringSplit4CharIterable.hasNext()) {
+                String s = stringSplit4CharIterable.next();
+            }
+        }
+        System.out.printf("%24s %7.2f ms%n", "new CharIterable", (System.nanoTime() - start) / 10000 / 100f);
     }
 
     @Test
@@ -61,27 +92,27 @@ public class StringSplitTest {
     public void stringSplitIterable() {
         long start = System.nanoTime();
         for (int i = 0; i < 1000000; i++) {
-            StringSplitIterable stringSplitIterable = new StringSplitIterable("mongo:key:2:test", delim);
+            StringSplitIterable stringSplitIterable = new StringSplitIterable(source, delim);
             while (stringSplitIterable.hasNext()) {
                 String s = stringSplitIterable.next();
             }
         }
-        System.out.printf("%20s %7.2f ms%n", "stringSplitIterable", (System.nanoTime() - start) / 10000 / 100f);
+        System.out.printf("%24s %7.2f ms%n", "stringSplitIterable", (System.nanoTime() - start) / 10000 / 100f);
     }
 
 
     @Test
     public void diySplit3() {
-        StringSplit stringSplit = new StringSplit("mongo:key:2:test", delim);
+        StringSplit stringSplit = new StringSplit(source, delim);
         System.out.println(stringSplit.first());
         System.out.println(stringSplit.next());
         System.out.println(stringSplit.last());
-        System.out.println(new StringSplit("mongo:key:2:test", delim).getList());
-        System.out.println(new StringSplit("mongo:key:2:test", delim).next());
+        System.out.println(new StringSplit(source, delim).getList());
+        System.out.println(new StringSplit(source, delim).next());
         System.out.println("=======================");
 
-        System.out.println(Arrays.toString("mongo:key:2:test".split("mongo")));
-        System.out.println(new StringSplit("mongo:key:2:test", "mongo").getList());
+        System.out.println(Arrays.toString(source.split("mongo")));
+        System.out.println(new StringSplit(source, "mongo").getList());
     }
 
 
