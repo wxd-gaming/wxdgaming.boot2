d1e07329a4b5a5db79fad040198b7571e2cd1022
wxd-gaming <492794628@qq.com>
2025-08-27 16:25:02 +0800

    【新增】convert使用案例

--- wxdgaming.boot2.starter.batis.sql.mysql/src/test/java/run/entity/EntityTest.java
+++ wxdgaming.boot2.starter.batis.sql.mysql/src/test/java/run/entity/EntityTest.java
@@ -2,8 +2,11 @@ package run.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import wxdgaming.boot2.starter.batis.ColumnType;
 import wxdgaming.boot2.starter.batis.EntityLongUID;
+import wxdgaming.boot2.starter.batis.ann.Convert;
 import wxdgaming.boot2.starter.batis.ann.DbColumn;
+import wxdgaming.boot2.starter.batis.convert.impl.Object2JsonStringConverter;
 import wxdgaming.boot2.starter.batis.sql.ann.Partition;
 
 import java.util.ArrayList;
@@ -22,6 +25,8 @@ public class EntityTest extends EntityLongUID {
     private int age;
     @DbColumn(length = 128)
     private String name;
+    @DbColumn(columnType = ColumnType.String, length = 2000)
+    @Convert(Object2JsonStringConverter.class)
     private final List<String> list = new ArrayList<>();
     private byte[] datas;
 }
