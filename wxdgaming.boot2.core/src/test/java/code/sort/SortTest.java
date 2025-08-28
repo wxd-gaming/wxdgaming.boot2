d90688da313b2d30ae6a71fb89c0bdd0dd1e5028
wxd-gaming <492794628@qq.com>
2025-08-28 14:25:59 +0800

    【修改】测试代码

--- wxdgaming.boot2.core/src/test/java/code/sort/SortTest.java
+++ wxdgaming.boot2.core/src/test/java/code/sort/SortTest.java
@@ -5,6 +5,7 @@ import wxdgaming.boot2.core.lang.DiffTimeRecord;
 import wxdgaming.boot2.core.util.RandomUtils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.TreeSet;
 
@@ -19,45 +20,82 @@ public class SortTest {
     static int count = 10000;
 
     public DiffTimeRecord newDiffTimeRecord() {
-        return DiffTimeRecord.start(DiffTimeRecord.IntervalConvertConst.US);
+        return DiffTimeRecord.start(DiffTimeRecord.IntervalConvertConst.MS);
     }
 
     @Test
     public void arraySort() {
-        ArrayList<SortBean> arrayList = null;
+        SortBean[] arrayList = new SortBean[count];
+        for (int i = 0; i < count; i++) {
+            SortBean sortBean = new SortBean();
+            sortBean.setKey(String.valueOf(i));
+            sortBean.setOverTime(System.currentTimeMillis() + RandomUtils.random(100, 10000));
+            arrayList[i] = sortBean;
+        }
+
         DiffTimeRecord diffTime = newDiffTimeRecord();
         for (int k = 0; k < 4; k++) {
-            arrayList = new ArrayList<>(count);
-            for (int i = 0; i < count; i++) {
-                SortBean sortBean = new SortBean();
-                sortBean.setKey(String.valueOf(i));
-                sortBean.setOverTime(System.currentTimeMillis() + RandomUtils.random(100, 10000));
-                arrayList.add(sortBean);
-            }
-            arrayList.sort(null);
+            SortBean[] sortBeans = Arrays.copyOf(arrayList, arrayList.length);
+            Arrays.sort(sortBeans);
             diffTime.marker("array add Sort");
+        }
+
+        for (int k = 0; k < 4; k++) {
+            SortBean[] sortBeans = Arrays.copyOf(arrayList, arrayList.length);
+            for (SortBean sortBean : sortBeans) {
+                sortBean.setOverTime(System.currentTimeMillis() + RandomUtils.random(100, 10000));
+            }
+            Arrays.sort(sortBeans);
+            diffTime.marker("array update all Sort");
+        }
+
+            SortBean[] sortBeans = Arrays.copyOf(arrayList, arrayList.length);
+        for (int k = 0; k < 4; k++) {
+            SortBean sortBean = RandomUtils.random(sortBeans);
+            sortBean.setOverTime(System.currentTimeMillis() + RandomUtils.random(100, 10000));
+            Arrays.sort(sortBeans);
+            diffTime.marker("array update one Sort");
+        }
+
+        System.out.println(diffTime.buildString4());
+
+    }
+
+    @Test
+    public void listSort() {
+        ArrayList<SortBean> arrayList = null;
+
+        arrayList = new ArrayList<>(count);
+        for (int i = 0; i < count; i++) {
+            SortBean sortBean = new SortBean();
+            sortBean.setKey(String.valueOf(i));
+            sortBean.setOverTime(System.currentTimeMillis() + RandomUtils.random(100, 10000));
+            arrayList.add(sortBean);
+        }
+
+        DiffTimeRecord diffTime = newDiffTimeRecord();
+        for (int k = 0; k < 4; k++) {
+            arrayList.sort(null);
+            diffTime.marker("list add Sort");
         }
 
         for (int k = 0; k < 4; k++) {
             List<SortBean> sortBeans = List.copyOf(arrayList);
             for (SortBean sortBean : sortBeans) {
                 sortBean.setOverTime(System.currentTimeMillis() + RandomUtils.random(100, 10000));
-                arrayList.sort(null);
             }
-            diffTime.marker("array update all Sort");
+            arrayList.sort(null);
+            diffTime.marker("list update all Sort");
         }
-
 
         for (int k = 0; k < 4; k++) {
             SortBean sortBean = RandomUtils.randomItem(arrayList);
             sortBean.setOverTime(System.currentTimeMillis() + RandomUtils.random(100, 10000));
             arrayList.sort(null);
-            diffTime.marker("array update one Sort");
+            diffTime.marker("list update one Sort");
         }
 
         System.out.println(diffTime.buildString());
-        System.out.println("===================================");
-        System.out.println(diffTime.buildString4());
 
     }
 
